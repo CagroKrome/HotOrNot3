fd0355bd43355fa9083b5421b34350cc560b7e84
Çağrı OKAN <cagriokan@akadmwin23012s-MacBook-Pro.local>
2022-06-20 01:23:07 +0300

    Responsive set

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Box from '../components/Box';
 import Navbar from '../components/Navbar';
+import '../index.css'
 
 
 function Home() {
   return (
     <>
     <Navbar home={'active'} />
-    <section className='app'>
+    <section className='home-app'>
       <Box />
     </section>
     </>
